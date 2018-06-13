d2845ba6e4b4e1933f8ad5c5d0d9bf70e431226d
Igor Skuhar <igorskuhar@gmail.com>
2018-06-13 17:53:34 +0200

    Added block assets

--- src/client/assets/index.js
+++ src/client/assets/index.js
@@ -1,5 +1,13 @@
-// import { settings } from 'pixi.js';
-//
-// const res = settings.RESOLUTION;
+import { settings } from 'pixi.js';
 
-export default [];
+const res = settings.RESOLUTION;
+
+let assets = [];
+
+if (res > 1) {
+  assets = [...assets, 'block@2x.png'];
+} else {
+  assets = [...assets, 'block.png'];
+}
+
+export default assets;
