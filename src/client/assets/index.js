1e5b5b262aec75956862ac242cc40f3292e0ce70
Igor Skuhar <igorskuhar@gmail.com>
2018-06-14 01:11:47 +0200

    Added raster assets
    block, grid

--- src/client/assets/index.js
+++ src/client/assets/index.js
@@ -1,13 +1,19 @@
-import { settings } from 'pixi.js';
-
-const res = settings.RESOLUTION;
-
 let assets = [];
 
-if (res > 1) {
-  assets = [...assets, 'block@2x.png'];
+if (window.devicePixelRatio > 1) {
+  assets = [
+    ...assets,
+    { name: 'block', url: 'assets/block@2x.png' },
+    { name: 'grid_inner', url: 'assets/grid_inner@2x.png' },
+    { name: 'grid_outer', url: 'assets/grid_outer@2x.png' }
+  ];
 } else {
-  assets = [...assets, 'block.png'];
+  assets = [
+    ...assets,
+    { name: 'block', url: 'assets/block.png' },
+    { name: 'grid_inner', url: 'assets/grid_inner.png' },
+    { name: 'grid_outer', url: 'assets/grid_outer.png' }
+  ];
 }
 
 export default assets;
