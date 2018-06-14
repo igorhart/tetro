400a88e09ed6a3e8d26a57b0a6a66913b7a6fd7a
Igor Skuhar <igorskuhar@gmail.com>
2018-06-14 20:18:08 +0200

    Updated Tetromino module

--- src/client/modules/Tetromino.js
+++ src/client/modules/Tetromino.js
@@ -1,23 +1,25 @@
 import { Container } from 'pixi.js';
 import Block from 'client/modules/Block';
-import { BLOCK_SIZE, BLOCK_GAP } from 'client/constants';
+import { BLOCK_SIZE, BLOCK_GAP, GRID_UNIT } from 'client/constants';
+import { degrees } from 'client/utils';
 
 export default class Tetromino extends Container {
-  constructor({ type, size, pivotPoint, states }) {
+  constructor({ type, size, pivotPoint, spawnVector, states }) {
     super();
 
     this._type = type;
     this._size = size;
     this._pivotPoint = pivotPoint;
+    this._spawnVector = spawnVector;
     this._states = states;
-    this._state = 0;
+    this._stateIndex = 0;
 
     this.addBlocks();
   }
 
   addBlocks() {
     // filter out empty rows
-    const state = this._states[this._state].filter(row => row.length);
+    const state = this.state.filter(row => row.some(cell => cell === 1));
 
     for (let rowIndex = 0; rowIndex < state.length; rowIndex += 1) {
       for (let colIndex = 0; colIndex < state[rowIndex].length; colIndex += 1) {
@@ -32,9 +34,247 @@ export default class Tetromino extends Container {
     }
 
     this.pivot.set(...this._pivotPoint);
-    this.cacheAsBitmap = true;
+  }
+
+  translate([x, y]) {
+    this.x += x * GRID_UNIT;
+    this.y += y * GRID_UNIT;
+  }
+
+  // clockwise
+  rotateCW() {
+    this._stateIndex += 1;
+    if (this._stateIndex > 3) {
+      this.stateIndex = 0;
+    }
+    this.rotation = degrees(this._stateIndex * 90);
+  }
+
+  // counter-clockwise
+  rotateCCW() {
+    this._stateIndex -= 1;
+    if (this._stateIndex < 0) {
+      this.stateIndex = 3;
+    }
+    this.rotation = degrees(this._stateIndex * 90);
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get spawnVector() {
+    return this._spawnVector;
+  }
+
+  get state() {
+    return this._states[this._stateIndex];
   }
 }
+
+// prettier-ignore
+export const types = {
+  I: {
+    type: 'I',
+    size: 4,
+    pivotPoint: [BLOCK_SIZE * 2 + BLOCK_GAP * 1.5, BLOCK_SIZE + BLOCK_GAP * 0.5],
+    spawnVector: [3, 1],
+    states: [
+      [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0]
+      ],
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0]
+      ],
+      [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+      ]
+    ]
+  },
+  J: {
+    type: 'J',
+    size: 3,
+    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
+    spawnVector: [3, 0],
+    states: [
+      [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ],
+      [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1]
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+      ]
+    ]
+  },
+  L: {
+    type: 'L',
+    size: 3,
+    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
+    spawnVector: [3, 0],
+    states: [
+      [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+      ],
+      [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+      ]
+    ]
+  },
+  O: {
+    type: 'O',
+    size: 2,
+    pivotPoint: [BLOCK_SIZE + BLOCK_GAP * 0.5, BLOCK_SIZE + BLOCK_GAP * 0.5],
+    spawnVector: [4, 0],
+    states: [
+      [
+        [1, 1],
+        [1, 1]
+      ],
+      [
+        [1, 1],
+        [1, 1]
+      ],
+      [
+        [1, 1],
+        [1, 1]
+      ],
+      [
+        [1, 1],
+        [1, 1]
+      ]
+    ]
+  },
+  S: {
+    type: 'S',
+    size: 3,
+    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
+    spawnVector: [3, 0],
+    states: [
+      [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+      ],
+      [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+      ],
+      [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+      ]
+    ]
+  },
+  T: {
+    type: 'T',
+    size: 3,
+    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
+    spawnVector: [3, 0],
+    states: [
+      [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+      ]
+    ]
+  },
+  Z: {
+    type: 'Z',
+    size: 3,
+    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
+    spawnVector: [3, 0],
+    states: [
+      [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+      ],
+      [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
+      ],
+      [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1]
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 0, 0]
+      ]
+    ]
+  }
+};
 
 /*
   translations to test when rotating
@@ -81,199 +321,5 @@ export const wallKicks = {
       0: [[0, 0], [1, 0], [-2, 0], [1, -2], [-2, 1]],
       2: [[0, 0], [-2, 0], [1, 0], [-2, -1], [1, 2]]
     }
-  }
-  // O does not kick
-};
-
-// prettier-ignore
-export const types = {
-  I: {
-    type: 'I',
-    size: 4,
-    pivotPoint: [BLOCK_SIZE * 2 + BLOCK_GAP * 1.5, BLOCK_SIZE + BLOCK_GAP * 0.5],
-    states: [
-      [
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-      ],
-      [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0]
-      ],
-      [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0]
-      ],
-      [
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0]
-      ]
-    ]
-  },
-  J: {
-    type: 'J',
-    size: 3,
-    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
-    states: [
-      [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-      ],
-      [
-        [0, 1, 1],
-        [0, 1, 0],
-        [0, 1, 0]
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 0, 1]
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 0]
-      ]
-    ]
-  },
-  L: {
-    type: 'L',
-    size: 3,
-    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
-    states: [
-      [
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 0]
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1]
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [1, 0, 0]
-      ],
-      [
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0]
-      ]
-    ]
-  },
-  O: {
-    type: 'O',
-    size: 2,
-    pivotPoint: [BLOCK_SIZE + BLOCK_GAP * 0.5, BLOCK_SIZE + BLOCK_GAP * 0.5],
-    states: [
-      [
-        [1, 1],
-        [1, 1]
-      ],
-      [
-        [1, 1],
-        [1, 1]
-      ],
-      [
-        [1, 1],
-        [1, 1]
-      ],
-      [
-        [1, 1],
-        [1, 1]
-      ]
-    ]
-  },
-  S: {
-    type: 'S',
-    size: 3,
-    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
-    states: [
-      [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0]
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1]
-      ],
-      [
-        [0, 0, 0],
-        [0, 1, 1],
-        [1, 1, 0]
-      ],
-      [
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0]
-      ]
-    ]
-  },
-  T: {
-    type: 'T',
-    size: 3,
-    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
-    states: [
-      [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0]
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 1, 0]
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 1, 0]
-      ],
-      [
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 1, 0]
-      ]
-    ]
-  },
-  Z: {
-    type: 'Z',
-    size: 3,
-    pivotPoint: [BLOCK_SIZE * 1.5 + BLOCK_GAP, BLOCK_SIZE * 1.5 + BLOCK_GAP],
-    states: [
-      [
-        [1, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
-      ],
-      [
-        [0, 0, 1],
-        [0, 1, 1],
-        [0, 1, 0]
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 0],
-        [0, 1, 1]
-      ],
-      [
-        [0, 1, 0],
-        [1, 1, 0],
-        [1, 0, 0]
-      ]
-    ]
   }
 };
