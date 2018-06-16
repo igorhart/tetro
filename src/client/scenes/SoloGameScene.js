65a77f34f7507db1bd3adc4122c9e385361889d4
Igor Skuhar <igorskuhar@gmail.com>
2018-06-16 18:39:01 +0200

    Added initial game structure

--- src/client/scenes/SoloGameScene.js
+++ src/client/scenes/SoloGameScene.js
@@ -4,72 +4,105 @@ import Bag from 'client/modules/Bag';
 import Scene from 'client/modules/Scene';
 import Tetromino from 'client/modules/Tetromino';
 import { types } from 'client/modules/Tetromino';
-import { BLOCK_GAP, GLOW_PADDING } from 'client/constants/dimensions';
-// import { TweenMax } from 'gsap/all';
-// import { randomInt } from 'client/utils';
 
 class SoloGameScene extends Scene {
   constructor({ id }) {
     super({ id });
 
-    this._bag = new Bag();
-
-    // this.addBackground();
+    this._gui = null;
+    this._blocksContainer = null;
     this.addGUI();
-    this.spawn();
+    this.reset();
+
+    this.start();
   }
 
-  // TODO
-  // addBackground() {}
+  start() {
+    // TODO: count down 3, 2, 1...
+    this.spawnTetromino();
+    // TODO:
+  }
+
+  pause() {
+    this.paused = true;
+    // TODO: blur this._gui
+    // TODO: show pause overlay
+  }
+
+  resume() {
+    // TODO: hide pause overlay
+    // TODO: count down 3, 2, 1...
+    // TODO: unblur this._gui
+    this.paused = false;
+  }
+
+  retry() {
+    this.reset();
+    this.start();
+  }
+
+  reset() {
+    this._bag = new Bag();
+
+    // TODO: destroy() all of them
+    this._tetromino = null;
+    this._tetrominoGhost = null;
+    this._nextTetromino = null;
+
+    this._score = 0;
+    this.highScore = 0; // TODO: get it from LocalStorage or set to zero
+    this._level = 0;
+    this._paused = false;
+  }
+
+  // gameOver() {
+  //
+  // }
+
+  spawnTetromino() {
+    if (this.nextTetromino) {
+      this._tetromino = this.nextTetromino;
+    } else {
+      this._tetromino = this.getRandomTetromino();
+    }
+    this._tetrominoGhost = this._tetromino.clone();
+    this._nextTetromino = this.getRandomTetromino();
+
+    this._blocksContainer.addChild(this._tetromino);
+    this._blocksContainer.addChild(this._nextTetromino);
+    this._nextContainer.addChild(this._nextTetromino);
+
+    this.moveTetromino(this._tetromino.spawnVector); // move to initial spawn position
+  }
+
+  getRandomTetromino() {
+    const tetromino = new Tetromino(types[this._bag.pick()]);
+    tetromino.position.set(tetromino.pivot.x, tetromino.pivot.y); // move to [0, 0]
+    return tetromino;
+  }
 
   addGUI() {
     // TODO: blur this._guiContainer when game paused
     // TODO: shake this._guiContainer when lines cleared (single = tiny shake, tetris = solid shake)
-    const guiContainer = new Container();
-    this._guiContainer = guiContainer;
-    this.addChild(guiContainer);
+    const gui = new Container();
+    this._gui = gui;
+    this.addChild(gui);
 
     const grid = new Grid();
     this._grid = grid;
-    guiContainer.addChild(grid);
+    gui.addChild(grid);
 
-    const blocksContainer = new Container();
-    this._blocksContainer = blocksContainer;
-    blocksContainer.x = GLOW_PADDING + BLOCK_GAP * 2;
-    blocksContainer.y = GLOW_PADDING + BLOCK_GAP * 2;
-    grid.addChild(blocksContainer);
+    this._blocksContainer = grid.getBlocksContainer();
 
-    this._guiContainer.position.set(
-      Math.floor(this.width / 2 - this._guiContainer.width / 2),
-      Math.floor(this.height / 2 - this._guiContainer.height / 2)
+    // TODO: add left sidebar
+    // TODO: add right sidebar
+    // TODO: center gui on window resize event
+
+    // center gui on the screen
+    this._gui.position.set(
+      Math.floor(this.width / 2 - this._gui.width / 2),
+      Math.floor(this.height / 2 - this._gui.height / 2)
     );
-  }
-
-  spawn() {
-    const tetromino = new Tetromino(types[this._bag.pick()]);
-    tetromino.position.set(tetromino.pivot.x, tetromino.pivot.y); // move to [0, 0]
-    tetromino.translate(tetromino.spawnVector); // move to initial spawn position
-    this._blocksContainer.addChild(tetromino);
-
-    setInterval(() => {
-      tetromino.rotateCW();
-    }, 420);
-
-    // TweenMax.to(tetromino, 0.6, { alpha: 0.7, yoyo: true, repeat: 100 });
-
-    // for (let i = 0; i < 7; i += 1) {
-    //   const tetromino = new Tetromino(types[this._bag.pick()]);
-    //   tetromino.position.set(tetromino.pivot.x, tetromino.pivot.y); // move to [0, 0]
-    //   tetromino.translate(tetromino.spawnVector); // move to initial spawn position
-    //
-    //   tetromino.translate([randomInt(-3, 4), i * 3]);
-    //   this._blocksContainer.addChild(tetromino);
-    // }
-
-    // const ghost = tetromino.clone();
-    // this._ghost = ghost;
-    // ghost.alpha = 0.5;
-    // this.addChild(ghost);
   }
 }
 
